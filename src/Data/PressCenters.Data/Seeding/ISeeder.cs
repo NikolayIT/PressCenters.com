cc0c66585d6fa564913262c467e6ea1626aa1675
Nikolay Kostov <admin@nikolay.it>
2018-12-23 13:53:42 +0200

    Seeders extracted into separate classes

---
+++ src/Data/PressCenters.Data/Seeding/ISeeder.cs
@@ -0,0 +1,9 @@
+﻿namespace PressCenters.Data.Seeding
+{
+    using System;
+
+    public interface ISeeder
+    {
+        void Seed(ApplicationDbContext dbContext, IServiceProvider serviceProvider);
+    }
+}
---
+++ src/Data/PressCenters.Data/Seeding/WorkerTasksSeeder.cs
@@ -0,0 +1,32 @@
+﻿namespace PressCenters.Data.Seeding
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using PressCenters.Data.Models;
+
+    public class WorkerTasksSeeder : ISeeder
+    {
+        public void Seed(ApplicationDbContext dbContext, IServiceProvider serviceProvider)
+        {
+            var workerTasks = new List<WorkerTask>
+                              {
+                                  new WorkerTask
+                                  {
+                                      TypeName = "PressCenters.Worker.Tasks.DbCleanupTask",
+                                      Parameters = "{}",
+                                      Priority = 0,
+                                  },
+                              };
+
+            foreach (var workerTask in workerTasks)
+            {
+                if (!dbContext.WorkerTasks.Any(x => x.TypeName == workerTask.TypeName))
+                {
+                    dbContext.WorkerTasks.Add(workerTask);
+                }
+            }
+        }
+    }
+}
