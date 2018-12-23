893212bea96ca78ffc35256bb59f6274738e4fce
Nikolay Kostov <admin@nikolay.it>
2018-12-23 13:42:51 +0200

    Added PressCenters.Worker.Tasks with DbCleanupTask

---
+++ src/Worker/PressCenters.Worker.Tasks/DbCleanupTask.cs
@@ -0,0 +1,49 @@
+﻿namespace PressCenters.Worker.Tasks
+{
+    using System;
+    using System.Threading.Tasks;
+
+    using Microsoft.Extensions.DependencyInjection;
+
+    using PressCenters.Data.Common;
+    using PressCenters.Data.Models;
+    using PressCenters.Worker.Common;
+
+    public class DbCleanupTask : BaseTask<DbCleanupTask.Input, DbCleanupTask.Output>
+    {
+        private readonly IDbQueryRunner queryRunner;
+
+        public DbCleanupTask(IServiceProvider serviceProvider)
+            : base(serviceProvider)
+        {
+            this.queryRunner = serviceProvider.GetService<IDbQueryRunner>();
+        }
+
+        protected override async Task<Output> DoWork(Input input)
+        {
+            await this.queryRunner.RunQueryAsync(
+                $"ALTER INDEX [PK_{nameof(News)}] ON [dbo].[{nameof(News)}] REBUILD;");
+            Console.WriteLine($"Index [PK_{nameof(News)}] rebuilt.");
+
+            await this.queryRunner.RunQueryAsync(
+                $"ALTER INDEX [PK_{nameof(MainNews)}] ON [dbo].[{nameof(MainNews)}] REBUILD;");
+            Console.WriteLine($"Index [PK_{nameof(MainNews)}] rebuilt.");
+
+            return new Output();
+        }
+
+        protected override WorkerTask Recreate(WorkerTask currentTask, Input parameters)
+        {
+            var runAfter = (currentTask.RunAfter ?? DateTime.UtcNow).AddDays(7).Date.AddHours(19); // 19:00 after 7 days
+            return new WorkerTask(currentTask, runAfter);
+        }
+
+        public class Input : BaseTaskInput
+        {
+        }
+
+        public class Output : BaseTaskOutput
+        {
+        }
+    }
+}
