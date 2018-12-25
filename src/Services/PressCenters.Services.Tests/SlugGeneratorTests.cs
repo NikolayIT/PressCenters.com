3c3a0582012447126af0ed7e99d9f168dc4c1f28
Nikolay Kostov <admin@nikolay.it>
2018-12-25 10:22:48 +0200

    PressCenters.Services.Tests created and added tests for the SlugGenerator

---
+++ src/Services/PressCenters.Services.Tests/SlugGeneratorTests.cs
@@ -0,0 +1,29 @@
+﻿namespace PressCenters.Services.Tests
+{
+    using PressCenters.Services;
+
+    using Xunit;
+
+    public class SlugGeneratorTests
+    {
+        [Fact]
+        public void GenerateSlugShouldTrimResult()
+        {
+            var input =
+                "Входът може да бъде много много много много много много много много много много много много много много много много много дълъг тест ";
+            var slugGenerator = new SlugGenerator();
+            var result = slugGenerator.GenerateSlug(input);
+            Assert.True(result.Length <= 100);
+        }
+
+        [Fact]
+        public void GenerateSlugShouldWorkCorrectly()
+        {
+            var input = "МВР: Свада в училище в ж.к.-„Западен парк“.";
+            var expected = "mvr-svada-v-uchilishte-v-jk-zapaden-park";
+            var slugGenerator = new SlugGenerator();
+            var result = slugGenerator.GenerateSlug(input);
+            Assert.Equal(expected, result);
+        }
+    }
+}
