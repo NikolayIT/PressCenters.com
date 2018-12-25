a2fb8aa5a8f850784dc0278a1f04c32ddca71119
Nikolay Kostov <admin@nikolay.it>
2018-12-25 18:23:05 +0200

    Installed and configured Application Insights

--- src/Web/PressCenters.Web/Program.cs
+++ src/Web/PressCenters.Web/Program.cs
@@ -11,6 +11,6 @@
         }
 
         private static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args).UseStartup<Startup>();
+            WebHost.CreateDefaultBuilder(args).UseStartup<Startup>().UseApplicationInsights();
     }
 }
