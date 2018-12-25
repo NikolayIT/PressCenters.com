c1bded3e55fd26fa3405ac0153ce5e7a9339e974
Nikolay Kostov <admin@nikolay.it>
2018-12-25 10:53:21 +0200

    Sources list page implemented

---
+++ src/Web/PressCenters.Web/Controllers/SourcesController.cs
@@ -0,0 +1,27 @@
+﻿namespace PressCenters.Web.Controllers
+{
+    using System.Linq;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    using PressCenters.Data.Common.Repositories;
+    using PressCenters.Data.Models;
+    using PressCenters.Services.Mapping;
+    using PressCenters.Web.ViewModels.Sources;
+
+    public class SourcesController : BaseController
+    {
+        private readonly IDeletableEntityRepository<Source> sourcesRepository;
+
+        public SourcesController(IDeletableEntityRepository<Source> sourcesRepository)
+        {
+            this.sourcesRepository = sourcesRepository;
+        }
+
+        public ActionResult List()
+        {
+            var sources = this.sourcesRepository.All().OrderBy(x => x.Name).To<SourceViewModel>().ToList();
+            return this.View(sources);
+        }
+    }
+}
---
+++ src/Web/PressCenters.Web/ViewModels/Sources/SourceViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace PressCenters.Web.ViewModels.Sources
+{
+    using PressCenters.Data.Models;
+    using PressCenters.Services.Mapping;
+
+    public class SourceViewModel : IMapFrom<Source>
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string ShortName { get; set; }
+
+        public string Description { get; set; }
+
+        public string Url { get; set; }
+    }
+}
