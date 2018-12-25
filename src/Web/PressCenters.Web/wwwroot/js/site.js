030e40bbeffda356410ba981339f4afdee3da60c
Nikolay Kostov <admin@nikolay.it>
2018-12-25 19:16:28 +0200

    Installed Moment.js and updated sidebar main news to use it for conversion utc to local time as well as visualizing the time in the local time zone.

--- src/Web/PressCenters.Web/wwwroot/js/site.js
+++ src/Web/PressCenters.Web/wwwroot/js/site.js
@@ -1,1 +1,7 @@
-﻿// Write your JavaScript code.
+﻿$(function () {
+    moment.locale("bg");
+    $('time').each(function (i, e) {
+        var time = moment.utc($(e).attr('datetime')).local();
+        $(e).html(time.format('llll'));
+    });
+});
