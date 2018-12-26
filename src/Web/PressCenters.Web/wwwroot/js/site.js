3cae2c43d69f86628554405e63fd3482cb89c26a
Nikolay Kostov <admin@nikolay.it>
2018-12-26 16:30:02 +0200

    Introduced _BaseLayout and improved admin area UI

--- src/Web/PressCenters.Web/wwwroot/js/site.js
+++ src/Web/PressCenters.Web/wwwroot/js/site.js
@@ -1,7 +1,8 @@
 ﻿$(function () {
     moment.locale("bg");
-    $('time').each(function (i, e) {
-        var time = moment.utc($(e).attr('datetime')).local();
-        $(e).html(time.format('llll'));
+    $("time").each(function (i, e) {
+        const time = moment.utc($(e).attr("datetime")).local();
+        $(e).html(time.format("llll"));
+        $(e).attr("title", $(e).attr("datetime"));
     });
 });
