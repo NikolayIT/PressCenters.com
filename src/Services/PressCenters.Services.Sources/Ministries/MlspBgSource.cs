1f3fe0157c2b61d7005704b962527908e42f2689
Nikolay Kostov <admin@nikolay.it>
2019-01-05 10:47:56 +0200

    Added new source: "Министерство на труда и социалната политика" (Closes #44)

---
+++ src/Services/PressCenters.Services.Sources/Ministries/MlspBgSource.cs
@@ -0,0 +1,64 @@
+﻿namespace PressCenters.Services.Sources.Ministries
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+
+    using AngleSharp.Dom;
+
+    /// <summary>
+    /// Министерство на труда и социалната политика.
+    /// </summary>
+    public class MlspBgSource : BaseSource
+    {
+        public override string BaseUrl { get; } = "https://www.mlsp.government.bg/";
+
+        public override IEnumerable<RemoteNews> GetLatestPublications() =>
+            this.GetPublications("index.php?section=PRESS", "#rub_co span a");
+
+        public override IEnumerable<RemoteNews> GetAllPublications()
+        {
+            for (var date = DateTime.UtcNow; date >= new DateTime(2014, 11, 1); date = date.AddDays(-1))
+            {
+                var news = this.GetPublications(
+                    $"index.php?section=PRESS2&vs=2&pr=3&lang=&dtT={date:dd}.{date:MM}.{date:yyyy}",
+                    "#statiata span a");
+                Console.WriteLine($"Date {date.ToShortDateString()} => {news.Count} news");
+                foreach (var remoteNews in news)
+                {
+                    remoteNews.PostDate = date.Date;
+                    yield return remoteNews;
+                }
+            }
+        }
+
+        public override string ExtractIdFromUrl(string url)
+        {
+            var matches = Regex.Matches(url, @"[\?&](([^&=]+)=([^&=#]*))", RegexOptions.Compiled);
+            var parameters = matches.Cast<Match>().ToDictionary(
+                m => Uri.UnescapeDataString(m.Groups[2].Value),
+                m => Uri.UnescapeDataString(m.Groups[3].Value));
+            return parameters["prid"];
+        }
+
+        protected override RemoteNews ParseDocument(IDocument document)
+        {
+            var titleElement = document.QuerySelector(".grid_2_3_long_bless h3");
+            var title = titleElement?.TextContent;
+
+            // No time provided in the news page
+            var time = DateTime.Now;
+
+            var imageElement = document.QuerySelector("#statiata img");
+            var imageUrl = imageElement?.GetAttribute("src") ?? "https://www.mlsp.government.bg/server/php/files/mtsp_rsz%20(1).jpg";
+
+            var contentElement = document.QuerySelector("#statiata");
+            this.RemoveRecursively(contentElement, imageElement);
+            this.NormalizeUrlsRecursively(contentElement, this.BaseUrl);
+            var content = contentElement?.InnerHtml;
+
+            return new RemoteNews(title, content, time, imageUrl);
+        }
+    }
+}
---
+++ src/Tests/PressCenters.Services.Sources.Tests/Ministries/MlspBgSourceTests.cs
@@ -0,0 +1,60 @@
+﻿namespace PressCenters.Services.Sources.Tests.Ministries
+{
+    using System.Linq;
+
+    using PressCenters.Services.Sources.Ministries;
+
+    using Xunit;
+
+    public class MlspBgSourceTests
+    {
+        [Theory]
+        [InlineData("https://www.mlsp.government.bg/index.php?section=PRESS2&prid=1576&lang=", "1576")]
+        [InlineData("https://www.mlsp.government.bg/index.php?section=PRESS2&prid=1572", "1572")]
+        public void ExtractIdFromPressUrlShouldWorkCorrectly(string url, string id)
+        {
+            var provider = new MlspBgSource();
+            var result = provider.ExtractIdFromUrl(url);
+            Assert.Equal(id, result);
+        }
+
+        [Fact]
+        public void ParseRemoteNewsShouldWorkCorrectly()
+        {
+            const string NewsUrl = "https://www.mlsp.government.bg/index.php?section=PRESS2&prid=1575";
+            var provider = new MlspBgSource();
+            var news = provider.GetPublication(NewsUrl);
+            Assert.Equal(NewsUrl, news.OriginalUrl);
+            Assert.Equal("Над 25 500 души ще получават услугите „Личен асистент“,  „Социален асистент“ и „Домашен помощник“ и през 2019 г.", news.Title);
+            Assert.Contains("Със Закона за държавния бюджет на Република България за 2019", news.Content);
+            Assert.Contains("без прекъсване в предоставянето на необходимата подкрепа.", news.Content);
+            Assert.DoesNotContain("mtsp_rsz%20(1).jpg", news.Content);
+            Assert.DoesNotContain("<img", news.Content);
+            Assert.Equal("https://www.mlsp.government.bg/server/php/files/1111 (4).jpg", news.ImageUrl);
+            Assert.Equal("1575", news.RemoteId);
+        }
+
+        [Fact]
+        public void ParseRemoteNewsWithoutImageShouldWorkCorrectly()
+        {
+            const string NewsUrl = "https://www.mlsp.government.bg/index.php?section=PRESS2&prid=87&lang=";
+            var provider = new MlspBgSource();
+            var news = provider.GetPublication(NewsUrl);
+            Assert.Equal(NewsUrl, news.OriginalUrl);
+            Assert.Equal("Над 12 300 безработни са започнали в реалната икономика в област Варна", news.Title);
+            Assert.Contains("За посочения период в бюрата по труда са заявени общо 10107 места", news.Content);
+            Assert.Contains("трудовите правоотношения и здравословните и безопасни условия на труд.", news.Content);
+            Assert.DoesNotContain("<img", news.Content);
+            Assert.Equal("https://www.mlsp.government.bg/server/php/files/mtsp_rsz (1).jpg", news.ImageUrl);
+            Assert.Equal("87", news.RemoteId);
+        }
+
+        [Fact]
+        public void GetNewsShouldReturnResults()
+        {
+            var provider = new MlspBgSource();
+            var result = provider.GetLatestPublications();
+            Assert.Equal(5, result.Count());
+        }
+    }
+}
