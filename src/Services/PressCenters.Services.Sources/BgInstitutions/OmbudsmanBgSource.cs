a83143056ad893f7636091638b8dcac9dd38d9c5
Nikolay Kostov <admin@nikolay.it>
2019-02-27 22:49:12 +0200

    Implemented news source: "Омбудсман" (Closes #65)

---
+++ src/Services/PressCenters.Services.Sources/BgInstitutions/OmbudsmanBgSource.cs
@@ -0,0 +1,104 @@
+﻿namespace PressCenters.Services.Sources.BgInstitutions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Text.RegularExpressions;
+
+    using AngleSharp.Dom;
+
+    /// <summary>
+    /// Омбудсман на Република България.
+    /// </summary>
+    public class OmbudsmanBgSource : BaseSource
+    {
+        public override string BaseUrl { get; } = "https://www.ombudsman.bg/";
+
+        public override IEnumerable<RemoteNews> GetLatestPublications() =>
+            this.GetPublications(string.Empty, "#leftColumn ul li a", count: 5);
+
+        public override IEnumerable<RemoteNews> GetAllPublications()
+        {
+            var lastKnownDate = new DateTime(2016, 1, 1);
+            for (var i = 1; i <= 5000; i++)
+            {
+                // Console.Title = i.ToString();
+                var remoteNews = this.GetPublication($"{this.BaseUrl}news/{i}");
+                if (remoteNews == null)
+                {
+                    continue;
+                }
+
+                if (remoteNews.PostDate.Date == DateTime.Now.Date)
+                {
+                    remoteNews.PostDate = lastKnownDate;
+                }
+
+                lastKnownDate = remoteNews.PostDate;
+                Console.WriteLine($"№{i} => {remoteNews.PostDate.ToShortDateString()} => {remoteNews.Title}");
+                yield return remoteNews;
+            }
+        }
+
+        protected override RemoteNews ParseDocument(IDocument document, string url)
+        {
+            // Title
+            var titleElement = document.QuerySelector(".news-content h3");
+            if (titleElement == null)
+            {
+                return null;
+            }
+
+            var title = titleElement.TextContent.Trim();
+
+            // Time
+            var timeElementAsString = string.Empty;
+
+            var strongElement = document.QuerySelector(".news-body strong");
+            if (strongElement?.TextContent?.Contains(" г.") == true && strongElement?.TextContent?.Length < 40)
+            {
+                timeElementAsString = strongElement.TextContent;
+            }
+            else
+            {
+                var newsAsText = document.QuerySelector(".news-body").TextContent.Trim();
+                var endOfString = newsAsText.IndexOf("г.", StringComparison.Ordinal);
+                if (endOfString > 0)
+                {
+                    timeElementAsString = newsAsText.Substring(0, endOfString + 2).Trim();
+                }
+            }
+
+            var timeAsString = timeElementAsString.Trim();
+            timeAsString = timeAsString.Replace("гр. София,", string.Empty);
+            timeAsString = Regex.Replace(timeAsString, @"[А-Яа-я]+\,\s*", string.Empty);
+            timeAsString = Regex.Replace(timeAsString, @"[А-Яа-я\s]+\(", string.Empty);
+            timeAsString = timeAsString.Replace("На ", string.Empty);
+            timeAsString = timeAsString.Replace("На ", string.Empty); // Not the same as the previous line
+            timeAsString = timeAsString.Replace("2099", "2009");
+            timeAsString = timeAsString.Replace("a", "а"); // Latin to cyrillic
+            timeAsString = timeAsString.Replace("e", "е"); // Latin to cyrillic
+            if (!DateTime.TryParseExact(
+                timeAsString.Trim(),
+                new[] { "d MMMM yyyy г.", "d MMMM yyyyг.", "dd MMMM yyyyг.", "dd.MM.yyyy г." },
+                new CultureInfo("bg-BG"),
+                DateTimeStyles.None,
+                out var time))
+            {
+                //// Console.WriteLine($"!!! {this.ExtractIdFromUrl(url)} -----> {timeElementAsString}");
+                time = DateTime.Now;
+            }
+
+            // Image
+            var imageUrl = document.QuerySelector(".news-content > .inline-image img")?.Attributes?["src"]?.Value
+                           ?? "/images/sources/ombudsman.bg.jpg";
+
+            // Content
+            var contentElement = document.QuerySelector(".news-content .news-body");
+            this.NormalizeUrlsRecursively(contentElement);
+            var content = contentElement?.InnerHtml;
+
+            return new RemoteNews(title, content, time, imageUrl);
+        }
+    }
+}
---
+++ src/Tests/PressCenters.Services.Sources.Tests/BgInstitutions/OmbudsmanBgSourceTests.cs
@@ -0,0 +1,65 @@
+﻿namespace PressCenters.Services.Sources.Tests.BgInstitutions
+{
+    using System;
+    using System.Linq;
+
+    using PressCenters.Services.Sources.BgInstitutions;
+
+    using Xunit;
+
+    public class OmbudsmanBgSourceTests
+    {
+        [Theory]
+        [InlineData("https://www.ombudsman.bg/news/1", "1")]
+        [InlineData("https://www.ombudsman.bg/news/2/", "2")]
+        [InlineData("https://www.ombudsman.bg/news/4984#middleWrapper", "4984")]
+        public void ExtractIdFromPressUrlShouldWorkCorrectly(string url, string id)
+        {
+            var provider = new OmbudsmanBgSource();
+            var result = provider.ExtractIdFromUrl(url);
+            Assert.Equal(id, result);
+        }
+
+        [Fact]
+        public void ParseRemoteNewsShouldWorkCorrectly()
+        {
+            const string NewsUrl = "https://www.ombudsman.bg/news/4979#middleWrapper";
+            var provider = new OmbudsmanBgSource();
+            var news = provider.GetPublication(NewsUrl);
+            Assert.Equal(NewsUrl, news.OriginalUrl);
+            Assert.Equal("Омбудсманът Мая Манолова представи законопроект за ограничаване на привилегиите на банки и монополисти", news.Title);
+            Assert.Contains("Омбудсманът Мая Манолова представи днес на обществена дискусия в институцията законопроект за изменения в Гражданския процесуален кодекс", news.Content);
+            Assert.Contains("Видео от изказването", news.Content);
+            Assert.DoesNotContain(news.Title, news.Content);
+            Assert.DoesNotContain(news.ImageUrl, news.Content);
+            Assert.Equal("https://www.ombudsman.bg//pictures/image.php?name=DSC_0028.JPG&w=242", news.ImageUrl);
+            Assert.Equal(new DateTime(2019, 2, 25), news.PostDate);
+            Assert.Equal("4979", news.RemoteId);
+        }
+
+        [Fact]
+        public void ParseRemoteNewsWithoutImageShouldWorkCorrectly()
+        {
+            const string NewsUrl = "https://www.ombudsman.bg/news/9";
+            var provider = new OmbudsmanBgSource();
+            var news = provider.GetPublication(NewsUrl);
+            Assert.Equal(NewsUrl, news.OriginalUrl);
+            Assert.Equal("Омбудсманът на Република България предлага промени в законопроекта за Общ устройствен план на София", news.Title);
+            Assert.Contains("Гиньо Ганев представи на председателя на Народното събрание Георги Пирински предложение", news.Content);
+            Assert.Contains("представители на специализираните архитектурните среди и на Министерството на регионалното развитие и благоустройството.", news.Content);
+            Assert.DoesNotContain(news.Title, news.Content);
+            Assert.DoesNotContain("<img", news.Content);
+            Assert.Equal("/images/sources/ombudsman.bg.jpg", news.ImageUrl);
+            Assert.Equal(new DateTime(2006, 10, 4), news.PostDate);
+            Assert.Equal("9", news.RemoteId);
+        }
+
+        [Fact]
+        public void GetLatestPublicationsShouldReturnResults()
+        {
+            var provider = new OmbudsmanBgSource();
+            var result = provider.GetLatestPublications();
+            Assert.Equal(5, result.Count());
+        }
+    }
+}
