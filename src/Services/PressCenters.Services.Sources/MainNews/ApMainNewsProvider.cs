e5c466aa317590d676e8231351265e78b0dc1d4f
Nikolay Kostov <admin@nikolay.it>
2019-08-24 16:13:09 +0300

    Implement Associated Press main news provider

---
+++ src/Services/PressCenters.Services.Sources/MainNews/ApMainNewsProvider.cs
@@ -0,0 +1,22 @@
+﻿namespace PressCenters.Services.Sources.MainNews
+{
+    public class ApMainNewsProvider : BaseMainNewsProvider
+    {
+        public override string BaseUrl { get; } = "https://www.apnews.com";
+
+        public override RemoteMainNews GetMainNews()
+        {
+            var document = this.GetDocument(this.BaseUrl);
+
+            var titleElement = document.QuerySelector("div[data-key='main-story'] a.headline");
+            var title = titleElement.TextContent.Trim();
+
+            var url = this.BaseUrl + titleElement.Attributes["href"].Value.Trim();
+
+            var imageElement = document.QuerySelector("div[data-key='main-story'] .LazyImage img");
+            var imageUrl = imageElement?.Attributes["src"]?.Value?.Trim();
+
+            return new RemoteMainNews(title, url, imageUrl);
+        }
+    }
+}
---
+++ src/Tests/PressCenters.Services.Sources.Tests/MainNews/ApMainNewsProviderTests.cs
@@ -0,0 +1,21 @@
+﻿namespace PressCenters.Services.Sources.Tests.MainNews
+{
+    using PressCenters.Services.Sources.MainNews;
+
+    using Xunit;
+
+    public class ApMainNewsProviderTests
+    {
+        [Fact]
+        public void GetMainNewsShouldWorkCorrectly()
+        {
+            var provider = new ApMainNewsProvider();
+            var news = provider.GetMainNews();
+            Assert.NotNull(news.Title);
+            Assert.True(news.Title.Length >= 10);
+            Assert.Contains("apnews.com", news.OriginalUrl);
+            Assert.StartsWith("https", news.OriginalUrl);
+            Assert.StartsWith("https", news.ImageUrl);
+        }
+    }
+}
