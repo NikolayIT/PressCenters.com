b79ff52c7a55afa2e8ef0a68aa4954073922d0ff
Nikolay Kostov <admin@nikolay.it>
2019-08-24 16:19:08 +0300

    Implement Reuters main news provider

---
+++ src/Services/PressCenters.Services.Sources/MainNews/ReutersMainNewsProvider.cs
@@ -0,0 +1,22 @@
+﻿namespace PressCenters.Services.Sources.MainNews
+{
+    public class ReutersMainNewsProvider : BaseMainNewsProvider
+    {
+        public override string BaseUrl { get; } = "https://www.reuters.com";
+
+        public override RemoteMainNews GetMainNews()
+        {
+            var document = this.GetDocument(this.BaseUrl);
+
+            var titleElement = document.QuerySelector("#topStory .story-title a");
+            var title = titleElement.TextContent.Trim();
+
+            var url = this.BaseUrl + titleElement.Attributes["href"].Value.Trim();
+
+            var imageElement = document.QuerySelector("#topStory .story-photo img");
+            var imageUrl = imageElement?.Attributes["src"]?.Value?.Trim();
+
+            return new RemoteMainNews(title, url, imageUrl);
+        }
+    }
+}
---
+++ src/Tests/PressCenters.Services.Sources.Tests/MainNews/ReutersMainNewsProviderTests.cs
@@ -0,0 +1,21 @@
+﻿namespace PressCenters.Services.Sources.Tests.MainNews
+{
+    using PressCenters.Services.Sources.MainNews;
+
+    using Xunit;
+
+    public class ReutersMainNewsProviderTests
+    {
+        [Fact]
+        public void GetMainNewsShouldWorkCorrectly()
+        {
+            var provider = new ReutersMainNewsProvider();
+            var news = provider.GetMainNews();
+            Assert.NotNull(news.Title);
+            Assert.True(news.Title.Length >= 10);
+            Assert.Contains("reuters.com", news.OriginalUrl);
+            Assert.StartsWith("https", news.OriginalUrl);
+            Assert.StartsWith("https", news.ImageUrl);
+        }
+    }
+}
