5fc99a08e3fb87a19c3e63d91729c3bb676ab7f2
Nikolay.IT <admin@nikolay.it>
2022-01-11 23:53:50 +0200

    Implement dark mode

--- src/Web/PressCenters.Web/wwwroot/js/site.js
+++ src/Web/PressCenters.Web/wwwroot/js/site.js
@@ -10,4 +10,14 @@
         $(e).html(time.format("llll"));
         $(e).attr("title", $(e).attr("datetime"));
     });
+
+    if (document.cookie.includes('darkMode=true')) {
+        document.getElementById("dark-mode").checked = 1;
+    }
 });
+
+function toggleDarkMode() {
+    document.documentElement.classList.toggle('dark-mode');
+    if (document.cookie.includes('darkMode=true')) document.cookie = 'darkMode=false';
+    else document.cookie = 'darkMode=true';
+}
