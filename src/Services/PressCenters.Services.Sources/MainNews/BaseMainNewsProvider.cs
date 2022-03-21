b4693ef6b8820e0812b80222861de0305c122783
Nikolay.IT <admin@nikolay.it>
2022-03-21 22:18:42 +0200

    Fix BtvNoviniteMainNewsProvider

--- src/Services/PressCenters.Services.Sources/MainNews/BaseMainNewsProvider.cs
+++ src/Services/PressCenters.Services.Sources/MainNews/BaseMainNewsProvider.cs
@@ -1,5 +1,6 @@
 ﻿namespace PressCenters.Services.Sources.MainNews
 {
+    using System;
     using System.Net.Http;
 
     using AngleSharp.Dom;
@@ -18,7 +19,10 @@
             var parser = new HtmlParser();
             var httpClient = new HttpClient();
             httpClient.DefaultRequestHeaders.Add("User-Agent", GlobalConstants.DefaultUserAgent);
-            var html = httpClient.GetStringAsync(url).GetAwaiter().GetResult();
+            //// httpClient.DefaultRequestHeaders.Add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9");
+            //// httpClient.DefaultRequestHeaders.Add("accept-language", "bg,en-US;q=0.9,en;q=0.8");
+            var request = httpClient.SendAsync(new HttpRequestMessage(HttpMethod.Get, url) { Version = new Version(2, 0) }).GetAwaiter().GetResult();
+            var html = request.Content.ReadAsStringAsync().GetAwaiter().GetResult();
             var document = parser.ParseDocument(html);
             return document;
         }
