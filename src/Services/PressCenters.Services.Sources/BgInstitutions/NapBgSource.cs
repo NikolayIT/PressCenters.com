722716b86ca1479de3b1ca2e44cb44b5defa0da5
Nikolay.IT <admin@nikolay.it>
2022-04-17 13:57:42 +0300

    Fixes in NapBgSource

--- src/Services/PressCenters.Services.Sources/BgInstitutions/NapBgSource.cs
+++ src/Services/PressCenters.Services.Sources/BgInstitutions/NapBgSource.cs
@@ -2,50 +2,38 @@
 {
     using System;
     using System.Collections.Generic;
-    using System.Globalization;
     using System.Linq;
 
     using AngleSharp.Dom;
 
-    using PressCenters.Common;
+    using Newtonsoft.Json;
 
     /// <summary>
     /// Национална агенция за приходите.
     /// </summary>
     public class NapBgSource : BaseSource
     {
-        public override string BaseUrl => "https://old.nra.bg/";
+        public override string BaseUrl => "https://nra.bg/";
+
+        public override bool UseProxy => true;
 
         public override IEnumerable<RemoteNews> GetLatestPublications()
         {
-            var document = this.Parser.ParseDocument(this.ReadStringFromUrl($"{this.BaseUrl}page?id=223"));
-            var links = document.QuerySelectorAll(".news_list li a").Select(x => x?.Attributes["onclick"]?.Value)
-                .Where(x => x?.Contains("ShowWindow('/news?id=") == true).Select(
-                    x => $"{this.BaseUrl}{x.GetStringBetween("ShowWindow('/", "'")}").ToList();
+            var json = this.ReadStringFromUrl($"{this.BaseUrl}customSearchWCM/query?context=nra.bg25863&libName=agency&saId=266cf85b-a315-4d1f-902b-180f72e9303f&atId=d89c331e-6555-473c-836d-2c5869933eb5&returnElements=category,image&filterByElements=&rPP=10&currentPage=1&rootPage=nra&returnProperties=title,publishDate&currentUrl=https%3A%2F%2Fnra.bg%2Fwps%2Fportal%2Fnra%2Factualno%2Factualno%2F&dateFormat=dd.MM.yyyy&ancestors=false&descendants=true&orderBy=publishDate&orderBy2=publishDate&orderBy3=title&sortOrder=false&searchTerm=&from=01.01.{DateTime.UtcNow.Year}&before=31.12.{DateTime.UtcNow.Year}&optionMeta=true&rand=0.707991665810006");
+            var newsAsJson = JsonConvert.DeserializeObject<IEnumerable<NapBgSource.NewsItemResponse>>(json);
+            var links = newsAsJson.Select(x => x.ContentUrl?.Url).Where(x => x != null).ToList();
+            if (!links.Any())
+            {
+                throw new Exception("No publications found.");
+            }
+
             var news = links.Select(this.GetPublication).Where(x => x != null).ToList();
             return news;
         }
 
-        public override IEnumerable<RemoteNews> GetAllPublications()
-        {
-            for (var i = 1; i <= 3850; i++)
-            {
-                var remoteNews = this.GetPublication($"{this.BaseUrl}news?id={i}");
-                if (remoteNews == null)
-                {
-                    continue;
-                }
-
-                Console.WriteLine($"№{i} => {remoteNews.PostDate.ToShortDateString()} => {remoteNews.Title}");
-                yield return remoteNews;
-            }
-        }
-
-        internal override string ExtractIdFromUrl(string url) => this.GetUrlParameterValue(url, "id");
-
         protected override RemoteNews ParseDocument(IDocument document, string url)
         {
-            var titleElement = document.QuerySelector("h1");
+            var titleElement = document.QuerySelector("h1.page-title");
             if (titleElement == null)
             {
                 return null;
@@ -53,25 +41,23 @@
 
             var title = titleElement.TextContent;
 
-            var timeElement = document.QuerySelector(".date");
-            if (timeElement == null)
-            {
-                return null;
-            }
-
-            var timeAsString = timeElement?.TextContent?.Trim().ToLower();
-            var time = DateTime.ParseExact(timeAsString, "dd MMMM yyyy", new CultureInfo("bg-BG"));
-
-            var contentElement = document.QuerySelector(".content");
+            var contentElement = document.QuerySelector("#richTextContainer");
             this.NormalizeUrlsRecursively(contentElement);
             var content = contentElement?.InnerHtml;
 
-            if (content?.Length < 50 || content.Contains("Р”Р°РЅСЉС†Рё"))
-            {
-                return null;
-            }
+            return new RemoteNews(title, content, DateTime.Now, null);
+        }
 
-            return new RemoteNews(title, content, time, null);
+        public class NewsItemResponse
+        {
+            [JsonProperty("contentUrl")]
+            public ContentUrl ContentUrl { get; set; }
+        }
+
+        public class ContentUrl
+        {
+            [JsonProperty("url")]
+            public string Url { get; set; }
         }
     }
 }
--- src/Tests/PressCenters.Services.Sources.Tests/BgInstitutions/NapBgSourceTests.cs
+++ src/Tests/PressCenters.Services.Sources.Tests/BgInstitutions/NapBgSourceTests.cs
@@ -10,8 +10,8 @@
     public class NapBgSourceTests
     {
         [Theory]
-        [InlineData("https://old.nra.bg/news?id=3843", "3843")]
-        [InlineData("https://old.nra.bg/news?id=337", "337")]
+        [InlineData("https://nra.bg/wps/portal/nra/actualno/NAP-nabludava-firmi-proizvoditeli-na-qica-i-agneshko", "NAP-nabludava-firmi-proizvoditeli-na-qica-i-agneshko")]
+        [InlineData("https://nra.bg/wps/portal/nra/actualno/Po-leki-antikovid-merki-v-ofisite-na-NAP", "Po-leki-antikovid-merki-v-ofisite-na-NAP")]
         public void ExtractIdFromPressUrlShouldWorkCorrectly(string url, string id)
         {
             var provider = new NapBgSource();
@@ -22,19 +22,17 @@
         [Fact]
         public void ParseRemoteNewsWithoutImageShouldWorkCorrectly()
         {
-            const string NewsUrl = "https://old.nra.bg/news?id=3829";
+            const string NewsUrl = "https://nra.bg/wps/portal/nra/actualno/NAP_zaporira_stoka_na_firma_ukrila_100_hil_lv_DDS";
             var provider = new NapBgSource();
             var news = provider.GetPublication(NewsUrl);
             Assert.Equal(NewsUrl, news.OriginalUrl);
-            Assert.Equal("Важно! Годишно приключване в НАП в периода 1-10 януари 2019 г.", news.Title);
-            Assert.Contains("В периода от 1 до 10 януари 2019 г. в НАП ще се извършват дейности", news.Content);
-            Assert.Contains("може да се получи на телефона на Информационния център на НАП 0700 18 700.", news.Content);
+            Assert.Equal("НАП запорира стока на фирма, укрила 100 хил. лв. ДДС", news.Title);
+            Assert.Contains("Национална агенция за приходите наложи запор върху стоки, собственост на фирма, укрила близо 100 хил. лв.", news.Content);
+            Assert.Contains("инициатива на НАП фирмата е с прекратена регистрация по Закона за ДДС.", news.Content);
             Assert.DoesNotContain(news.Title, news.Content);
-            Assert.DoesNotContain("03 Януари 2019", news.Content);
             Assert.DoesNotContain("<img", news.Content);
             Assert.Null(news.ImageUrl);
-            Assert.Equal(new DateTime(2019, 1, 3), news.PostDate);
-            Assert.Equal("3829", news.RemoteId);
+            Assert.Equal("NAP_zaporira_stoka_na_firma_ukrila_100_hil_lv_DDS", news.RemoteId);
         }
 
         [Fact]
