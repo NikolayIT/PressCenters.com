4ce03074ce7ce360bfc7d9f18be0aa5fb9b53e0f
Nikolay.IT <admin@nikolay.it>
2022-06-11 18:41:43 +0300

    Add new source: CemBgSource (Съвет за електронни медии - СЕМ)

---
+++ src/Services/PressCenters.Services.Sources/BgInstitutions/CemBgSource.cs
@@ -0,0 +1,61 @@
+﻿namespace PressCenters.Services.Sources.BgInstitutions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+
+    using AngleSharp.Dom;
+
+    public class CemBgSource : BaseSource
+    {
+        public override string BaseUrl => "https://www.cem.bg/";
+
+        public override IEnumerable<RemoteNews> GetLatestPublications() =>
+            this.GetPublications("newsbg", ".boxNews a", count: 5, urlShouldContain: "displaynewsbg");
+
+        public override IEnumerable<RemoteNews> GetAllPublications()
+        {
+            for (var page = 1; page <= 34; page++)
+            {
+                var news = this.GetPublications($"newsbg/page-{page}", ".boxNews a", urlShouldContain: "displaynewsbg");
+                Console.WriteLine($"Page {page} => {news.Count} news");
+                foreach (var remoteNews in news)
+                {
+                    yield return remoteNews;
+                }
+            }
+        }
+
+        protected override RemoteNews ParseDocument(IDocument document, string url)
+        {
+            var titleElement = document.QuerySelector(".articleEntry h3");
+            if (titleElement == null)
+            {
+                return null;
+            }
+
+            var title = titleElement.TextContent.Trim();
+
+            var contentElement = document.QuerySelector(".articleEntryDescription");
+
+            var imageElement = contentElement.QuerySelector("img");
+            var imageUrl = imageElement?.GetAttribute("src");
+            if (imageUrl?.EndsWith("images/file.png") == true)
+            {
+                imageElement = null;
+                imageUrl = null;
+            }
+
+            var timeElement = contentElement.QuerySelector(".date");
+            var time = DateTime.ParseExact(timeElement?.TextContent.ToLower().Trim(), "dd MMMM yyyy", CultureInfo.GetCultureInfo("bg-BG"));
+
+            contentElement.RemoveRecursively(imageElement);
+            contentElement.RemoveRecursively(timeElement);
+            contentElement.RemoveRecursively(contentElement.QuerySelector("script"));
+            this.NormalizeUrlsRecursively(contentElement);
+            var content = contentElement.InnerHtml.Trim();
+
+            return new RemoteNews(title, content, time, imageUrl);
+        }
+    }
+}
---
+++ src/Tests/PressCenters.Services.Sources.Tests/BgInstitutions/CemBgSourceTests.cs
@@ -0,0 +1,68 @@
+﻿namespace PressCenters.Services.Sources.Tests.BgInstitutions
+{
+    using System;
+    using System.Linq;
+
+    using PressCenters.Services.Sources.BgInstitutions;
+
+    using Xunit;
+
+    public class CemBgSourceTests
+    {
+        [Theory]
+        [InlineData("https://www.cem.bg/displaynewsbg/810", "810")]
+        [InlineData("https://www.cem.bg/displaynewsbg/809/", "809")]
+        public void ExtractIdFromUrlShouldWorkCorrectly(string url, string id)
+        {
+            var provider = new CemBgSource();
+            var result = provider.ExtractIdFromUrl(url);
+            Assert.Equal(id, result);
+        }
+
+        [Fact]
+        public void ParseRemoteNewsShouldWorkCorrectly()
+        {
+            const string NewsUrl = "https://www.cem.bg/displaynewsbg/672";
+            var provider = new CemBgSource();
+            var news = provider.GetPublication(NewsUrl);
+            Assert.Equal(NewsUrl, news.OriginalUrl);
+            Assert.Equal("Прессъобщение", news.Title);
+            Assert.Equal("672", news.RemoteId);
+            Assert.Equal(new DateTime(2020, 7, 10), news.PostDate);
+            Assert.Contains("Съветът за електронни медии на 9 юли 2020 г. се срещна с посланика на САЩ", news.Content);
+            Assert.Contains("предложение да споделят експертния потенциал на своите държави с България.", news.Content);
+            Assert.Contains("https://www.cem.bg/files/images/IMG_2047.JPG", news.Content);
+            Assert.DoesNotContain("Прессъобщение", news.Content);
+            Assert.DoesNotContain("10 Юли 2020", news.Content);
+            Assert.DoesNotContain("javascript", news.Content);
+            Assert.DoesNotContain("IMG_2020", news.Content);
+            Assert.Equal("https://www.cem.bg/files/images/IMG_2020 %281%29.JPG", news.ImageUrl);
+        }
+
+        [Fact]
+        public void ParseRemoteNewsWithoutAnImageShouldWorkCorrectly()
+        {
+            const string NewsUrl = "https://www.cem.bg/displaynewsbg/822";
+            var provider = new CemBgSource();
+            var news = provider.GetPublication(NewsUrl);
+            Assert.Equal(NewsUrl, news.OriginalUrl);
+            Assert.Equal("Прессъобщение", news.Title);
+            Assert.Equal("822", news.RemoteId);
+            Assert.Equal(new DateTime(2022, 6, 9), news.PostDate.Date);
+            Assert.Contains("По инициатива на Съвета за електронни медии се проведе среща с изпълнителния директор", news.Content);
+            Assert.Contains("във връзка с прилагане на Закона за хазарта, в частта реклама на хазартни игри.", news.Content);
+            Assert.DoesNotContain("Прессъобщение", news.Content);
+            Assert.DoesNotContain("09 Юни 2022", news.Content);
+            Assert.DoesNotContain("javascript", news.Content);
+            Assert.Null(news.ImageUrl);
+        }
+
+        [Fact]
+        public void GetNewsShouldReturnResults()
+        {
+            var provider = new CemBgSource();
+            var result = provider.GetLatestPublications();
+            Assert.Equal(5, result.Count());
+        }
+    }
+}
