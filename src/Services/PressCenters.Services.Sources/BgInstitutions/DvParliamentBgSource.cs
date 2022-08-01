453d78acd91729f20c9e9e61b3a4a0f5ac2dc656
Nikolay.IT <admin@nikolay.it>
2022-08-01 21:20:43 +0300

    Add new source "Държавен вестник" (DvParliamentBgSource)

---
+++ src/Services/PressCenters.Services.Sources/BgInstitutions/DvParliamentBgSource.cs
@@ -0,0 +1,41 @@
+﻿namespace PressCenters.Services.Sources.BgInstitutions
+{
+    using System;
+    using System.Collections.Generic;
+
+    using AngleSharp.Dom;
+
+    /// <summary>
+    /// Държавен вестник.
+    /// </summary>
+    public class DvParliamentBgSource : BaseSource
+    {
+        public override string BaseUrl => "https://dv.parliament.bg/";
+
+        public override IEnumerable<RemoteNews> GetLatestPublications()
+        {
+            var document = this.Parser.ParseDocument(this.ReadStringFromUrl($"{this.BaseUrl}DVWeb/index.faces"));
+            var title = document.QuerySelector(".tdHead1").TextContent.Trim();
+            var contentElement = document.QuerySelector("#index_form\\:dataTable1");
+            var images = contentElement.QuerySelectorAll("img");
+            foreach (var image in images)
+            {
+                contentElement.RemoveRecursively(image);
+            }
+
+            this.NormalizeUrlsRecursively(contentElement);
+            var content = contentElement.OuterHtml;
+            content = content.Replace("showMaterialDV.jsp", "DVWeb/showMaterialDV.jsp");
+            var news = new RemoteNews(title, content, DateTime.Now, null)
+            {
+                RemoteId = title,
+            };
+            yield return news;
+        }
+
+        protected override RemoteNews ParseDocument(IDocument document, string url)
+        {
+            return null;
+        }
+    }
+}
---
+++ src/Tests/PressCenters.Services.Sources.Tests/BgInstitutions/DvParliamentBgSourceTests.cs
@@ -0,0 +1,26 @@
+﻿namespace PressCenters.Services.Sources.Tests.BgInstitutions
+{
+    using System;
+    using System.Linq;
+
+    using PressCenters.Services.Sources.BgInstitutions;
+
+    using Xunit;
+
+    public class DvParliamentBgSourceTests
+    {
+        [Fact]
+        public void GetLatestPublicationsShouldReturnCurrentIssue()
+        {
+            var provider = new DvParliamentBgSource();
+            var result = provider.GetLatestPublications();
+            Assert.Single(result);
+            var news = result.First();
+            Assert.NotNull(news.Title);
+            Assert.NotNull(news.Content);
+            Assert.Contains("Брой: ", news.Title);
+            Assert.Contains(" от дата ", news.Title);
+            Assert.Contains("Преглед на материала", news.Content);
+        }
+    }
+}
