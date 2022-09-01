2af66a35b44492fc6d020a88426caba5e27f5674
Nikolay.IT <admin@nikolay.it>
2022-09-01 21:02:07 +0300

    Fix OmbudsmanBgSource

--- src/Services/PressCenters.Services.Sources/BgInstitutions/OmbudsmanBgSource.cs
+++ src/Services/PressCenters.Services.Sources/BgInstitutions/OmbudsmanBgSource.cs
@@ -17,87 +17,27 @@
         public override bool UseProxy => true;
 
         public override IEnumerable<RemoteNews> GetLatestPublications() =>
-            this.GetPublications(string.Empty, "#leftColumn ul li a", count: 5);
-
-        public override IEnumerable<RemoteNews> GetAllPublications()
-        {
-            var lastKnownDate = new DateTime(2016, 1, 1);
-            for (var i = 1; i <= 5000; i++)
-            {
-                // Console.Title = i.ToString();
-                var remoteNews = this.GetPublication($"{this.BaseUrl}news/{i}");
-                if (remoteNews == null)
-                {
-                    continue;
-                }
-
-                if (remoteNews.PostDate.Date == DateTime.Now.Date)
-                {
-                    remoteNews.PostDate = lastKnownDate;
-                }
-
-                lastKnownDate = remoteNews.PostDate;
-                Console.WriteLine($"№{i} => {remoteNews.PostDate.ToShortDateString()} => {remoteNews.Title}");
-                yield return remoteNews;
-            }
-        }
+            this.GetPublications("bg/p/novini", ".m-title a", count: 5);
 
         protected override RemoteNews ParseDocument(IDocument document, string url)
         {
-            // Title
-            var titleElement = document.QuerySelector(".news-content h3");
-            if (titleElement == null)
-            {
-                return null;
-            }
-
+            var titleElement = document.QuerySelector(".m-article h2");
             var title = titleElement.TextContent.Trim();
 
-            // Time
-            var timeElementAsString = string.Empty;
+            var timeElement = document.QuerySelector(".m-article .text-muted");
+            var timeAsString = timeElement.TextContent.Split(",")[1].Trim();
+            var time = DateTime.ParseExact(timeAsString, "dd.MM.yyyy", CultureInfo.InvariantCulture);
 
-            var strongElement = document.QuerySelector(".news-body strong");
-            if (strongElement?.TextContent?.Contains(" г.") == true && strongElement?.TextContent?.Length < 40)
-            {
-                timeElementAsString = strongElement.TextContent;
-            }
-            else
-            {
-                var newsAsText = document.QuerySelector(".news-body").TextContent.Trim();
-                var endOfString = newsAsText.IndexOf("г.", StringComparison.Ordinal);
-                if (endOfString > 0)
-                {
-                    timeElementAsString = newsAsText.Substring(0, endOfString + 2).Trim();
-                }
-            }
+            var imageElement = document.QuerySelector(".m-article .m-news-image");
+            var imageUrl = imageElement?.Attributes?["src"]?.Value;
 
-            var timeAsString = timeElementAsString.Trim();
-            timeAsString = timeAsString.Replace("гр. София,", string.Empty);
-            timeAsString = Regex.Replace(timeAsString, @"[А-Яа-я]+\,\s*", string.Empty);
-            timeAsString = Regex.Replace(timeAsString, @"[А-Яа-я\s]+\(", string.Empty);
-            timeAsString = timeAsString.Replace("На ", string.Empty);
-            timeAsString = timeAsString.Replace("На ", string.Empty); // Not the same as the previous line
-            timeAsString = timeAsString.Replace("2099", "2009");
-            timeAsString = timeAsString.Replace("a", "а"); // Latin to cyrillic
-            timeAsString = timeAsString.Replace("e", "е"); // Latin to cyrillic
-            if (!DateTime.TryParseExact(
-                timeAsString.Trim(),
-                new[] { "d MMMM yyyy г.", "d MMMM yyyyг.", "dd MMMM yyyyг.", "dd.MM.yyyy г." },
-                new CultureInfo("bg-BG"),
-                DateTimeStyles.None,
-                out var time))
-            {
-                //// Console.WriteLine($"!!! {this.ExtractIdFromUrl(url)} -----> {timeElementAsString}");
-                time = DateTime.Now;
-            }
-
-            // Image
-            var imageUrl = document.QuerySelector(".news-content > .inline-image img")?.Attributes?["src"]?.Value;
-
-            // Content
-            var contentElement = document.QuerySelector(".news-content .news-body");
+            var contentElement = document.QuerySelector(".m-article");
+            contentElement.RemoveRecursively(titleElement);
+            contentElement.RemoveRecursively(timeElement);
+            contentElement.RemoveRecursively(document.QuerySelector(".m-article a"));
+            contentElement.RemoveRecursively(document.QuerySelector(".m-article .justify-content-center"));
             this.NormalizeUrlsRecursively(contentElement);
-            var content = contentElement?.InnerHtml;
+            var content = contentElement.InnerHtml.Trim();
 
             return new RemoteNews(title, content, time, imageUrl);
         }
--- src/Tests/PressCenters.Services.Sources.Tests/BgInstitutions/OmbudsmanBgSourceTests.cs
+++ src/Tests/PressCenters.Services.Sources.Tests/BgInstitutions/OmbudsmanBgSourceTests.cs
@@ -10,9 +10,8 @@
     public class OmbudsmanBgSourceTests
     {
         [Theory]
-        [InlineData("https://www.ombudsman.bg/news/1", "1")]
-        [InlineData("https://www.ombudsman.bg/news/2/", "2")]
-        [InlineData("https://www.ombudsman.bg/news/4984#middleWrapper", "4984")]
+        [InlineData("https://www.ombudsman.bg/bg/n/ombudsmanat-poiska-nezabavni-merki-za-pred-1877", "ombudsmanat-poiska-nezabavni-merki-za-pred-1877")]
+        [InlineData("https://www.ombudsman.bg/bg/n/ombudsmanat-voda-v-svoge-veche-ima-no-vodna-1876/", "ombudsmanat-voda-v-svoge-veche-ima-no-vodna-1876")]
         public void ExtractIdFromPressUrlShouldWorkCorrectly(string url, string id)
         {
             var provider = new OmbudsmanBgSource();
@@ -23,37 +22,19 @@
         [Fact]
         public void ParseRemoteNewsShouldWorkCorrectly()
         {
-            const string NewsUrl = "https://www.ombudsman.bg/news/4979#middleWrapper";
+            const string NewsUrl = "https://www.ombudsman.bg/bg/n/eksperti-na-ombudsmana-diana-kovacheva-dnes-1881";
             var provider = new OmbudsmanBgSource();
             var news = provider.GetPublication(NewsUrl);
             Assert.NotNull(news);
             Assert.Equal(NewsUrl, news.OriginalUrl);
-            Assert.Equal("Омбудсманът Мая Манолова представи законопроект за ограничаване на привилегиите на банки и монополисти", news.Title);
-            Assert.Contains("Омбудсманът Мая Манолова представи днес на обществена дискусия в институцията законопроект за изменения в Гражданския процесуален кодекс", news.Content);
-            Assert.Contains("Видео от изказването", news.Content);
+            Assert.Equal("Експерти на омбудсмана Диана Ковачева днес дават приемна за граждани в Пловдив", news.Title);
+            Assert.Contains("Екип от експерти на омбудсмана Диана Ковачева ще консултират днес", news.Content);
+            Assert.Contains("Търговище, Разград, Силистра, Шумен, Видин, Враца, Стара Загора, Сливен, Смолян и Пловдив", news.Content);
             Assert.DoesNotContain(news.Title, news.Content);
             Assert.DoesNotContain(news.ImageUrl, news.Content);
-            Assert.Equal("https://www.ombudsman.bg//pictures/image.php?name=DSC_0028.JPG&w=242", news.ImageUrl);
-            Assert.Equal(new DateTime(2019, 2, 25), news.PostDate);
-            Assert.Equal("4979", news.RemoteId);
-        }
-
-        [Fact]
-        public void ParseRemoteNewsWithoutImageShouldWorkCorrectly()
-        {
-            const string NewsUrl = "https://www.ombudsman.bg/news/9";
-            var provider = new OmbudsmanBgSource();
-            var news = provider.GetPublication(NewsUrl);
-            Assert.NotNull(news);
-            Assert.Equal(NewsUrl, news.OriginalUrl);
-            Assert.Equal("Омбудсманът на Република България предлага промени в законопроекта за Общ устройствен план на София", news.Title);
-            Assert.Contains("Гиньо Ганев представи на председателя на Народното събрание Георги Пирински предложение", news.Content);
-            Assert.Contains("представители на специализираните архитектурните среди и на Министерството на регионалното развитие и благоустройството.", news.Content);
-            Assert.DoesNotContain(news.Title, news.Content);
-            Assert.DoesNotContain("<img", news.Content);
-            Assert.Null(news.ImageUrl);
-            Assert.Equal(new DateTime(2006, 10, 4), news.PostDate);
-            Assert.Equal("9", news.RemoteId);
+            Assert.Equal("https://www.ombudsman.bg/storage//pub/gallery/20220831122734_Diana Kovatcheva.JPG", news.ImageUrl);
+            Assert.Equal(new DateTime(2022, 8, 31), news.PostDate);
+            Assert.Equal("eksperti-na-ombudsmana-diana-kovacheva-dnes-1881", news.RemoteId);
         }
 
         [Fact]
