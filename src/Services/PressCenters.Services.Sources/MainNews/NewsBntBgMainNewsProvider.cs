db813825e41901b08140a2d19a3729dd48301d34
Nikolay.IT <admin@nikolay.it>
2022-12-10 17:17:23 +0200

    Fix NewsBntBgMainNewsProvider

--- src/Services/PressCenters.Services.Sources/MainNews/NewsBntBgMainNewsProvider.cs
+++ src/Services/PressCenters.Services.Sources/MainNews/NewsBntBgMainNewsProvider.cs
@@ -6,15 +6,15 @@
 
         public override RemoteMainNews GetMainNews()
         {
-            var document = this.GetDocument(this.BaseUrl + "/bg");
+            var document = this.GetDocument(this.BaseUrl);
 
-            var titleElement = document.QuerySelector(".top-img h1.home-news-title");
+            var titleElement = document.QuerySelector(".main-news-title");
             var title = titleElement.TextContent.Trim();
 
-            var urlElement = document.QuerySelector(".top-img .img-wrap a");
+            var urlElement = document.QuerySelector(".main-news-title");
             var url = urlElement.Attributes["href"].Value.Trim();
 
-            var imageElement = document.QuerySelector(".top-img .img-wrap img");
+            var imageElement = document.QuerySelector(".top-img img");
             var imageUrl = imageElement?.Attributes["src"]?.Value?.Trim();
 
             return new RemoteMainNews(title, url, imageUrl);
