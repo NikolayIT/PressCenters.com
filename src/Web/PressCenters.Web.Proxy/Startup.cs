3a9bd053eeee10230395ab1203a3e5db9bfa2ea5
Nikolay.IT <admin@nikolay.it>
2023-01-14 21:27:02 +0200

    Improvements in PressCenters.Web.Proxy

--- src/Web/PressCenters.Web.Proxy/Startup.cs
+++ src/Web/PressCenters.Web.Proxy/Startup.cs
@@ -1,5 +1,9 @@
 ﻿namespace PressCenters.Web.Proxy
 {
+    using System;
+    using System.Net.Sockets;
+    using System.Net;
+    using System.Text;
     using System.Threading.Tasks;
 
     using Microsoft.AspNetCore.Builder;
@@ -7,6 +11,7 @@
     using Microsoft.AspNetCore.Http;
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Hosting;
+    using System.Net.Http;
 
     public class Startup
     {
@@ -21,6 +26,20 @@
                 app.UseDeveloperExceptionPage();
             }
 
+            app.Map("/version", (builder) =>
+            {
+                builder.Run(async (context) =>
+                {
+                    StringBuilder output = new StringBuilder();
+                    output.AppendLine($"ProcessorCount: {Environment.ProcessorCount}");
+                    output.AppendLine($"WorkingSet: {Environment.WorkingSet}");
+                    output.AppendLine($"OSVersion: {Environment.OSVersion}");
+                    output.AppendLine($"Version: {Environment.Version}");
+                    string externalIpString = (await new HttpClient().GetStringAsync("http://icanhazip.com")).Trim();
+                    output.AppendLine($"IP: {externalIpString}");
+                    await context.Response.WriteAsync(output.ToString());
+                });
+            });
             app.UseMiddleware<ReverseProxyMiddleware>();
 
             app.Run(context =>
