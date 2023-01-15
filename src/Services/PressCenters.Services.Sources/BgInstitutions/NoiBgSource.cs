6b57e3d8e0a4f403ab429b656e2b6275cebf0195
Nikolay.IT <admin@nikolay.it>
2023-01-15 20:40:18 +0200

    Fixes in NoiBgSource

--- src/Services/PressCenters.Services.Sources/BgInstitutions/NoiBgSource.cs
+++ src/Services/PressCenters.Services.Sources/BgInstitutions/NoiBgSource.cs
@@ -3,51 +3,24 @@
     using System;
     using System.Collections.Generic;
     using System.Globalization;
-    using System.Net;
-    using System.Text.RegularExpressions;
 
     using AngleSharp.Dom;
-
-    using PressCenters.Common;
 
     /// <summary>
     /// Национален осигурителен институт.
     /// </summary>
     public class NoiBgSource : BaseSource
     {
-        public override string BaseUrl { get; } = "http://www.noi.bg/";
-
-        protected override List<(string Header, string Value)> Headers =>
-            new List<(string Header, string Value)>
-            {
-                ("Cookie", "cb379c5a647843b32707848c89ea970d=bg-BG"),
-            };
+        public override string BaseUrl { get; } = "https://nssi.bg/";
 
         public override IEnumerable<RemoteNews> GetLatestPublications() =>
-            this.GetPublications("newsbg", "h2.nssi-postheader a", count: 5);
+            this.GetPublications("publichnost/novini/", ".post-article h4 a", count: 5);
 
         public override IEnumerable<RemoteNews> GetAllPublications()
         {
-            for (var year = 2011; year <= 2017; year++)
+            for (var page = 1; page <= 25; page++)
             {
-                for (var page = 1; page <= 15; page++)
-                {
-                    var remoteNews = this.GetPublications(
-                        $"newsbg/{year}?start={(page - 1) * 5}",
-                        "h2.nssi-postheader a");
-                    Console.WriteLine($"{year} / {page} => {remoteNews.Count}");
-                    foreach (var news in remoteNews)
-                    {
-                        yield return news;
-                    }
-                }
-            }
-
-            for (var page = 1; page <= 2; page++)
-            {
-                var remoteNews = this.GetPublications(
-                    $"newsbg?start={(page - 1) * 40}",
-                    "h2.nssi-postheader a");
+                var remoteNews = this.GetPublications($"publichnost/novini/page/{page}/", ".post-article h4 a");
                 Console.WriteLine($"Page {page} => {remoteNews.Count}");
                 foreach (var news in remoteNews)
                 {
@@ -56,41 +29,20 @@
             }
         }
 
-        internal override string ExtractIdFromUrl(string url) => url.GetLastStringBetween("/", "-");
-
         protected override RemoteNews ParseDocument(IDocument document, string url)
         {
-            var titleElement = document.QuerySelector("h2.nssi-postheader");
-            if (titleElement == null)
-            {
-                return null;
-            }
+            var titleElement = document.QuerySelector("#main-content h1");
+            var title = titleElement.TextContent.Trim();
 
-            var title = titleElement.TextContent;
+            var timeAsString = document.QuerySelector("meta[property='article:published_time']").GetAttribute("content");
+            var time = DateTime.Parse(timeAsString, CultureInfo.InvariantCulture);
 
-            var timeElement = document.QuerySelector(".nssi-postdateicon");
-            var timeAsString = timeElement?.TextContent?.Trim().ToLower();
-            DateTime time;
-            if (timeAsString == "сряда, 30 ноември -0001 02:00")
-            {
-                var regex = new Regex(@"d{4}");
-                var match = regex.Match(title);
-                var year = match.Groups[0].ToString();
-                time = new DateTime(year != string.Empty ? year.ToInteger() : 2013, 12, 1);
-            }
-            else
-            {
-                time = DateTime.ParseExact(timeAsString, "dddd, dd MMMM yyyy HH:mm", new CultureInfo("bg-BG"));
-            }
+            var imageElement = document.QuerySelector("meta[property='og:image']");
+            var imageUrl = imageElement?.GetAttribute("content");
 
-            var imageElement = document.QuerySelector(".nssi-article img[src*='News']");
-            var imageUrl = imageElement?.GetAttribute("src");
-
-            var contentElement = document.QuerySelector(".nssi-article");
-            contentElement.RemoveRecursively(imageElement);
-            contentElement.RemoveRecursively(contentElement.QuerySelector("ul.pagenav"));
+            var contentElement = document.QuerySelector(".post-content");
             this.NormalizeUrlsRecursively(contentElement);
-            var content = contentElement.InnerHtml;
+            var content = contentElement?.InnerHtml;
 
             return new RemoteNews(title, content, time, imageUrl);
         }
--- src/Tests/PressCenters.Services.Sources.Tests/BgInstitutions/NoiBgSourceTests.cs
+++ src/Tests/PressCenters.Services.Sources.Tests/BgInstitutions/NoiBgSourceTests.cs
@@ -10,8 +10,8 @@
     public class NoiBgSourceTests
     {
         [Theory]
-        [InlineData("http://www.noi.bg/newsbg/5373-statscontactcenter", "5373")]
-        [InlineData("http://www.noi.bg/newsbg/2011/1-2011-11-27-17-38-26", "1")]
+        [InlineData("https://nssi.bg/nssi_sspf_meeting_10102022/", "nssi_sspf_meeting_10102022")]
+        [InlineData("https://nssi.bg/udostoverenia-za-pro-shte-se-izdavatdo-21-dekemvri-2022", "udostoverenia-za-pro-shte-se-izdavatdo-21-dekemvri-2022")]
         public void ExtractIdFromPressUrlShouldWorkCorrectly(string url, string id)
         {
             var provider = new NoiBgSource();
@@ -22,40 +22,19 @@
         [Fact]
         public void ParseRemoteNewsShouldWorkCorrectly()
         {
-            const string NewsUrl = "http://www.noi.bg/newsbg/5364-dohoddecember2018";
+            const string NewsUrl = "https://nssi.bg/sumata-za-nedostigasht-mesec-osiguritelen0staj0e-bez-promiana/";
             var provider = new NoiBgSource();
             var news = provider.GetPublication(NewsUrl);
             Assert.Equal(NewsUrl, news.OriginalUrl);
-            Assert.Equal("Средният осигурителен доход за страната за ноември 2018 г. е 917,00 лева", news.Title);
-            Assert.Contains("Националният осигурителен институт обявява, че размерът на средния осигурителен доход", news.Content);
-            Assert.Contains("месец декември 2018 г., съгласно чл. 70, ал. 2 от Кодекса за социално осигуряване.", news.Content);
+            Assert.Equal("Сумата за недостигащ месец осигурителен стаж е без промяна – 140,58 лв.", news.Title);
+            Assert.Contains("Без промяна от началото на 2023 г. остава сумата, която лицата внасят за така нареченото закупуване на осигурителен стаж", news.Content);
+            Assert.Contains("е 3840, а средният недостигащ стаж – 22,18 месеца.", news.Content);
             Assert.DoesNotContain(news.Title, news.Content);
-            Assert.DoesNotContain("10 Януари 2019", news.Content);
-            Assert.DoesNotContain("<img", news.Content);
-            Assert.DoesNotContain("noi_12.jpg", news.Content);
-            Assert.DoesNotContain("Предишна", news.Content);
-            Assert.Equal("http://www.noi.bg/images/bg/News/images/noi_12.jpg", news.ImageUrl);
-            Assert.Equal(new DateTime(2019, 1, 10, 10, 2, 0), news.PostDate);
-            Assert.Equal("5364", news.RemoteId);
-        }
-
-        [Fact]
-        public void ParseRemoteNewsWithoutImageShouldWorkCorrectly()
-        {
-            const string NewsUrl = "http://www.noi.bg/newsbg/2011/2-2011-11-27-17-38-37";
-            var provider = new NoiBgSource();
-            var news = provider.GetPublication(NewsUrl);
-            Assert.Equal(NewsUrl, news.OriginalUrl);
-            Assert.Equal("Заявление за потвърждаване на осигурителни периоди, придобити по законодателството на бившата ГДР.", news.Title);
-            Assert.Contains("Заявление за потвърждаване на осигурителни периоди, придобити по законодателството на бившата ГДР.", news.Content);
-            Assert.Contains("http://www.noi.bg/images/bg/users/forms/ermd/Zaqvl_potv_na_osig_periodi_GDR.doc", news.Content);
-            Assert.Contains("/images/icons/doc.gif", news.Content);
-            Assert.DoesNotContain("27 Ноември 2011", news.Content);
-            Assert.DoesNotContain("Предишна", news.Content);
-            Assert.DoesNotContain("Посещения", news.Content);
-            Assert.Null(news.ImageUrl);
-            Assert.Equal(new DateTime(2011, 11, 27, 19, 18, 0), news.PostDate);
-            Assert.Equal("2", news.RemoteId);
+            Assert.DoesNotContain("pension_1.jpg", news.Content);
+            Assert.DoesNotContain("януари 4, 2023", news.Content);
+            Assert.Equal("https://nssi.bg/wp-content/uploads/pension_1.jpg", news.ImageUrl);
+            Assert.Equal(new DateTime(2023, 1, 4, 10, 4, 9), news.PostDate);
+            Assert.Equal("sumata-za-nedostigasht-mesec-osiguritelen0staj0e-bez-promiana", news.RemoteId);
         }
 
         [Fact]
